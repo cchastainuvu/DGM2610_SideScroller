c45c6c86305fc9c5c96c0c5c70b202536817eef1
Unknown <10798368@my.uvu.edu>
2019-01-30 14:18:17 -0700

    project setup, file organization

---
+++ DGM2610_SideScrollGame/Assets/Scripts/AutoRunner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//References: CatLikeCoding, with own generic modification.
+//XX minutes of work/troubleshooting
+public class AutoRunner : MonoBehaviour 
+{
+
+
+}

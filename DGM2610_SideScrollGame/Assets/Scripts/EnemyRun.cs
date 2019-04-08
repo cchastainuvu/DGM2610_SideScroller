ed7bbfb826914247de46e0d2a8a5a0bf8fb3538c
Unknown <10798368@my.uvu.edu>
2019-04-08 15:40:55 -0600

    EnemyRun, hierarchy fully organized

---
+++ DGM2610_SideScrollGame/Assets/Scripts/EnemyRun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRun : MonoBehaviour
+{
+    public FloatData EnemySpeed, SlowSpeed, SpeedReset;
+
+    public void Start()
+    {
+        SpeedReset = EnemySpeed;
+    }
+    
+    private void FixedUpdate()
+    {
+        transform.Translate(EnemySpeed.Value * Time.deltaTime, 0F, 0F);
+    }
+
+    public void Slow()
+    {
+        EnemySpeed = SlowSpeed;
+    }
+
+    public void ResetSpeed()
+    {
+        EnemySpeed = SpeedReset;
+    }
+}

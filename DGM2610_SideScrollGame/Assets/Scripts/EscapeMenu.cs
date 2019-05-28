324feea1e97b18181eea87322231d6823eb44a19
Unknown <10798368@my.uvu.edu>
2019-05-27 22:12:27 -0600

    build settings, ui/menu

---
+++ DGM2610_SideScrollGame/Assets/Scripts/EscapeMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EscapeMenu : MonoBehaviour
+{
+
+   public UnityEvent EscapeEvent;
+
+   private void FixedUpdate()
+   {
+      if (Input.GetKeyDown(KeyCode.Escape))
+      {
+         EscapeEvent.Invoke();
+      }
+   }
+}
--- DGM2610_SideScrollGame/Assets/Scripts/SceneMGR.cs
+++ DGM2610_SideScrollGame/Assets/Scripts/SceneMGR.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class SceneMGR : MonoBehaviour
@@ -16,4 +17,5 @@ public class SceneMGR : MonoBehaviour
     {
         Application.Quit();
     }
+    
 }
